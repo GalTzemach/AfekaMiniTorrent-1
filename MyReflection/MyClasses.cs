901359b9535727e2fd1c34e119a0eb4ba941cfc6
Tal <talzemah1@gmail.com>
2018-03-19 16:41:16 +0200

    Add MyReflection project.

---
+++ MyReflection/MyClasses.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReflection
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class Author : Attribute
+    {
+        public string Name { get; set; }
+
+        public Author(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    struct MyStruct
+    {
+        string str;
+        public string Str { get => str; set => str = value; }
+    }
+
+    [Serializable]
+    [Author("Gal")]
+    public class Class1
+    {
+
+    }
+
+    [Serializable]
+    [Author("Tal")]
+    public class Class2
+    {
+
+    }
+
+    [Serializable]
+    [Author("Gal"), Author("Tal")]
+    public class Class3
+    {
+
+    }
+
+    [Serializable]
+    public class Class4
+    {
+
+    }
+}
